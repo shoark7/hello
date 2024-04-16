36a2a921d2b1cb78ae135765b68b8db4bd7baf2a
Park Sunghwan <shoark7@gmail.com>
2024-04-16 22:35:36 +0900

    Initialize test go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.20
+
+require github.com/shoark7/greetings v1.0.0 // indirect
---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shoark7/greetings"
+)
+
+func main() {
+	fmt.Println("Hello..!")
+
+	fmt.Println(greetings.Hello("heyboy!!"))
+}
